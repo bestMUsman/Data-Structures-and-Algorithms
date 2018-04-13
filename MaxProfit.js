f3bd84cdba095dc08457d0be3ce84eb009d0efb8
Muhammad Usman <musmanraodev@gmail.com>
2018-04-13 14:55:35 -0400

    Update MaxProfit.js

--- MaxProfit.js
+++ MaxProfit.js
@@ -1,13 +1,31 @@
-// Best Time to Buy and Sell Stock (leetcode)
-const maxProfit = function(prices) {
-  let maxValue = 0;
-  let minValue = Math.max(...prices);
+/*
+Say you have an array for which the ith element is the price of a given stock on day i.
 
-  for (let i = 0; i < prices.length; i++) {
-    minValue = Math.min(minValue, prices[i]);
-    maxValue = Math.max(maxValue, prices[i] - minValue);
+If you were only permitted to complete at most one transaction (ie, buy one and sell one share of the stock), design an algorithm to find the maximum profit.
+
+Example 1:
+Input: [7, 1, 5, 3, 6, 4]
+Output: 5
+max. difference = 6-1 = 5 (not 7-1 = 6, as selling price needs to be larger than buying price)
+
+Example 2:
+Input: [7, 6, 4, 3, 1]
+Output: 0
+In this case, no transaction is done, i.e. max profit = 0.
+*/
+
+/**
+ * @param {number[]} prices
+ * @return {number}
+**/
+
+const maxProfit = function(arr) {
+  let min = arr[0], profit = 0;
+  for (let i = 1; i < arr.length; i++) {
+    min = Math.min(min, arr[i]);
+    profit = Math.max(arr[i]-min, profit)
   }
-  return maxValue;
+  return profit;
 };
 
 maxProfit([7, 1, 5, 3, 6, 4]);
