8396cc98637156219a02fcf382d1d435642776ec
Muhammad Usman <musmanraodev@gmail.com>
2018-05-27 19:38:58 -0400

    Update dropElements.js

--- dropElements.js
+++ dropElements.js
@@ -1,4 +1,5 @@
 /*
+https://www.freecodecamp.org/challenges/drop-it
 Drop the elements of an array (first argument), starting from the front, until the predicate (second argument) returns true.
 
 The second argument, func, is a function you'll use to test the first elements of the array to decide if you should drop it or not.
