f5386a085c5059261058fa547f187aa64aa80642
Muhammad Usman <musmanraodev@gmail.com>
2019-07-18 14:08:44 -0400

    Update ValidParenthesis.js

--- ValidParenthesis.js
+++ ValidParenthesis.js
@@ -19,3 +19,35 @@ const isValidParenthesis = function(s) {
 let a = "()[]{}"; // true
 let b = "([[])"; // false
 isValidParenthesis(a);
+
+// Updated version:
+function isValidParentheses(str) {
+    let validOpenMap = {
+        "{": "}",
+        "[": "]",
+        "(": ")"
+    };
+
+    let validCloseMap = {
+        "}": true,
+        ")": true,
+        "]": true,
+    };
+
+    let stack = [];
+
+    for (let i = 0; i < str.length; i++) {
+        if (validOpenMap[str[i]]) {
+            stack.push(validOpenMap[str[i]]);
+        } else if (validCloseMap[str[i]] && stack.pop() !== str[i]) {
+            return false;
+        }
+    }
+
+    return stack.length === 0;
+}
+
+console.log(isValidParentheses("[{dsfdsfdasdas}{}adsfdsfdsf}]") === false);
+console.log(isValidParentheses("sd[{dsfdsdas}dsf{dsf{adsf}adsfdsf}]") === true);
+console.log(isValidParentheses("[({})]") === true); 
+console.log(isValidParentheses("[(])") === false);
